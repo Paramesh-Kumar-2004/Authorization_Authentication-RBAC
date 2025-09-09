f531c4fb959be767d71f0e41f2133dd9ca38adcb
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-09 21:56:13 +0530

    Writing GetUser Route

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-const { registerUser, loginUser } = require("../Controllers/UserControllers")
+const { registerUser, loginUser, getUsers } = require("../Controllers/UserControllers")
 
 
 
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
-
+router.route("/getUser").get(getUsers)
 
 
 module.exports = router
