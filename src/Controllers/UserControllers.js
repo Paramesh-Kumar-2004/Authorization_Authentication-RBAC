b83f132741ef376af0d4335b874d8cf0266eaed1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-13 19:17:46 +0530

    Writing createUser Controller And Route

--- src/Controllers/UserControllers.js
+++ src/Controllers/UserControllers.js
@@ -98,30 +98,35 @@ const getUsers = CatchAsyncError(async (req, res, next) => {
 })
 
 const createUser = CatchAsyncError(async (req, res, next) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-        next(new ErrorHandler("Enter All The Fields", 400))
-    }
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            next(new ErrorHandler("Enter All The Fields", 400))
+        }
 
-    const isExist = await User.findOne({ email })
-    if (isExist) {
-        return res.status(404).json({
-            "message": "This Email Already Exist",
-            isExist
+        const isExist = await User.findOne({ email })
+        if (isExist) {
+            return res.status(404).json({
+                "message": "This Email Already Exist",
+                isExist
+            })
+        }
+
+        const user = await User.create({ name, email, password })
+        if (!user) {
+            return res.status(400).json({
+                "message": "Something Went Wrong... Try Again Later"
+            })
+        }
+
+        return res.status(200).json({
+            "message": "User Create Successfully",
+            user
         })
     }
-
-    const user = await User.create({ name, email, password })
-    if (!user) {
-        return res.status(400).json({
-            "message": "Something Went Wrong... Try Again Later"
-        })
+    catch (error) {
+        next(new ErrorHandler(error.message, 505))
     }
-    
-    return res.status(200).json({
-        "message": "User Create Successfully",
-        user
-    })
 
 })
 
